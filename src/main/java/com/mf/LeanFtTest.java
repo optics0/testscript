33561ef2776331ced7a71472440f45f305c0dfff
Ramesh Nagarajan <ramesh.nagarajan@hpe.com>
2017-11-30 19:39:08 +0000

    First Commit

--- src/main/java/com/mf/LeanFtTest.java
+++ src/main/java/com/mf/LeanFtTest.java
@@ -67,7 +67,6 @@ public class LeanFtTest extends UnitTestClassBase {
 
         browser.sync();
 
-
         browser.describe(Image.class, new ImageDescription.Builder()
                 .alt("")
                 .tagName("IMG")
